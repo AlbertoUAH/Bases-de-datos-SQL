54179dca1c0ecaad1cb368517c70b2b6f054d61e
Alberto Fernandez Hernandez <45654081+AlbertoUAH@users.noreply.github.com>
2020-10-20 09:03:42 +0200

    Hechas las consultas (falta revision)

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,5 @@
+USE apps_moviles;
+
 -- Consulta 1. Obtener la fecha en la que se realizan mas descargas
 SELECT FEC_DESCARGA, count(FEC_DESCARGA) as NUM_DESCARGAS
 FROM descarga
@@ -84,4 +86,42 @@ GROUP BY aplicacion.nombre
 ORDER BY MESES, DESCARGAS DESC
 LIMIT 1;
 
--- Consulta 13. 
+-- Consulta 13. Consultar empleados con entre 1 y 3 annos de experiencia, cuyas aplicaciones en las que hayan participado tengan una media de puntuacion mayor a 3
+SELECT distinct(empleado.DNI), avg(descarga.PUNTUACION) as MEDIA
+FROM empleado INNER JOIN trabaja ON empleado.DNI = trabaja.DNI
+INNER JOIN realiza ON empleado.DNI = realiza.DNI 
+INNER JOIN aplicacion ON realiza.NOMBRE = aplicacion.NOMBRE
+INNER JOIN descarga ON aplicacion.NOMBRE = descarga.NOMBRE
+WHERE timestampdiff(year, trabaja.FECHA_INI, trabaja.FECHA_FIN) BETWEEN 1 AND 3
+GROUP BY empleado.DNI
+HAVING MEDIA > 3;
+
+-- Consulta 14. Consultar las aplicaciones realizadas entre los años 2013 y 2015, cuyo espacio en memoria no supere los 20 MB y el porcentaje de descargas sea mayor al 10 %
+SELECT distinct(aplicacion.NOMBRE)
+FROM aplicacion INNER JOIN descarga ON aplicacion.NOMBRE = descarga.NOMBRE
+WHERE year(aplicacion.FECHA_INI) BETWEEN 2012 AND 2016 AND year(aplicacion.FECHA_FIN) BETWEEN 2012 AND 2016 AND aplicacion.NOMBRE IN
+(SELECT descarga.NOMBRE
+FROM descarga
+GROUP BY descarga.NOMBRE
+HAVING count(*) * 100 / (SELECT count(*) FROM usuario) > 60);
+
+-- Consulta 15. Consultar empleados de entre 2 y 4 annos de experiencia en el desarrollo de aplicaciones de "Belleza", junto con
+-- empleados que hayan participado en mas de un proyecto de aplicaciones de "Entretenimiento"
+(SELECT distinct(empleado.DNI)
+FROM trabaja INNER JOIN empresa ON trabaja.VAT = empresa.VAT
+INNER JOIN empleado ON trabaja.DNI = empleado.DNI
+INNER JOIN realiza ON empleado.DNI = realiza.DNI
+INNER JOIN aplicacion ON realiza.NOMBRE = aplicacion.NOMBRE
+INNER JOIN categorias_aplicacion ON aplicacion.NOMBRE = categorias_aplicacion.NOMBRE
+INNER JOIN categorias ON categorias_aplicacion.ID_CATEGORIA = categorias.ID_CATEGORIA
+WHERE timestampdiff(year, trabaja.FECHA_INI, trabaja.FECHA_FIN) BETWEEN 2 AND 4 AND categorias.NOMBRE = 'Belleza')
+
+UNION
+
+(SELECT distinct(realiza.DNI)
+FROM realiza INNER JOIN aplicacion ON realiza.NOMBRE = aplicacion.NOMBRE
+INNER JOIN categorias_aplicacion ON aplicacion.NOMBRE = categorias_aplicacion.NOMBRE
+INNER JOIN categorias ON categorias_aplicacion.ID_CATEGORIA = categorias.ID_CATEGORIA
+WHERE categorias.NOMBRE = 'Entretenimiento'
+GROUP BY realiza.DNI
+HAVING count(realiza.DNI) > 1);
